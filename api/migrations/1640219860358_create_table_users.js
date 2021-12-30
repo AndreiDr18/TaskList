408f339983a18a8f3567d9dc4ccb2d0635ccab2c
AndreiDr18 <dragneaandrei2001@gmail.com>
2021-12-30 14:19:43 +0200

    Fixed migrations typo

--- api/migrations/1640219860358_create_table_users.js
+++ api/migrations/1640219860358_create_table_users.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "up": "CREATE TABLE users2 (UUID TEXT, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)",
-    "down": "DROP TABLE users2"
+    "up": "CREATE TABLE users (UUID TEXT, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)",
+    "down": "DROP TABLE users"
 }
--- api/migrations/1640219875289_create_table_tasks.js
+++ api/migrations/1640219875289_create_table_tasks.js
@@ -1,4 +1,4 @@
 module.exports = {
-    "up": "CREATE TABLE tasks2 (name VARCHAR(255) NOT NULL, description TEXT, dueDate DATE, status ENUM('ToDo','InProgress','Done','Failed'), tags VARCHAR(255))",
-    "down": "DROP TABLE tasks2"
+    "up": "CREATE TABLE tasks (name VARCHAR(255) NOT NULL, description TEXT, dueDate DATE, status ENUM('ToDo','InProgress','Done','Failed'), tags VARCHAR(255))",
+    "down": "DROP TABLE tasks"
 }
